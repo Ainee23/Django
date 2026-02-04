f10ce870ad66289ae602820408e8b1a2bb5a0955
Ainee23 <aineemakwana23@gmail.com>
2026-02-04 11:52:42 +0530

    4 feb 2026 orm

--- learning26/student/admin.py
+++ learning26/student/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import studentinfo, product, games
 # Register your models here.
+admin.site.register(studentinfo)
+admin.site.register(product)
+admin.site.register(games)
--- learning26/student/models.py
+++ learning26/student/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class studentinfo(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    city=models.CharField(max_length=100)
+    email=models.EmailField(null=True)
+    class meta:
+        db_table="studentinfo"
+    
+class product(models.Model):
+    product_name=models.CharField(max_length=100)
+    price=models.FloatField()
+    quantity=models.IntegerField()
+    color=models.CharField(null=True)
+    status=models.CharField(default=True)
+
+    class meta:
+        db_table="product"
+    
+class games(models.Model):
+    game_name=models.CharField(max_length=100)
+    genre=models.CharField(max_length=100)
+    platform=models.CharField(max_length=100)
+    price=models.FloatField(null=True)
+
+    class meta:
+        db_table="games"
